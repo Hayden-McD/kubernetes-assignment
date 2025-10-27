440868236afcb84fbb40956d128817210da94de1
hayden-mcd <mcdohr2@student.op.ac.nz>
2025-10-27 16:42:10 +1300

    Added dockerfiles for ngingx and flask app. Created simple flask app using templates.

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    # Renders templates/homepage.html
+    return render_template("homepage.html")
+
+# This runs only if the file is run using python
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
