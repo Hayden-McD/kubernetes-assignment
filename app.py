ce9c9887e0e2d80c90b26f7bc776c54a05495684
Ubuntu <Hayden@Hayden-VM.ttxhuseizofuhawkpd5gxpzyjf.px.internal.cloudapp.net>
2025-10-30 00:43:36 +0000

    Added example secret config. Added secret to gitignore

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ import socket
 
 app = Flask(__name__)
 
+# Defaults to 'redis' and '6379' if environment variables are not set or running locally
 redis_host = os.getenv("REDIS_HOST", "redis")
 redis_port = int(os.getenv("REDIS_PORT", "6379"))
+redis_password = os.getenv("REDIS_PASSWORD")
 
 db = redis.Redis(
     host=redis_host,
     port=redis_port,
+    password=redis_password,
     db=0,
     decode_responses=True
 )
